3e7e566be423c40a013e3a7ec9cc307b464176f4
kouheisatou <koheisato0420@gmail.com>
2024-12-05 14:54:56 +0900

    ADD : pages on mvvm

---
+++ frontend/lib/model/project_state.freezed.dart
@@ -0,0 +1,162 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'project_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$ProjectState {
+  String get title => throw _privateConstructorUsedError;
+  String get image => throw _privateConstructorUsedError;
+
+  /// Create a copy of ProjectState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ProjectStateCopyWith<ProjectState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProjectStateCopyWith<$Res> {
+  factory $ProjectStateCopyWith(
+          ProjectState value, $Res Function(ProjectState) then) =
+      _$ProjectStateCopyWithImpl<$Res, ProjectState>;
+  @useResult
+  $Res call({String title, String image});
+}
+
+/// @nodoc
+class _$ProjectStateCopyWithImpl<$Res, $Val extends ProjectState>
+    implements $ProjectStateCopyWith<$Res> {
+  _$ProjectStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of ProjectState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? image = null,
+  }) {
+    return _then(_value.copyWith(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      image: null == image
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ProjectStateImplCopyWith<$Res>
+    implements $ProjectStateCopyWith<$Res> {
+  factory _$$ProjectStateImplCopyWith(
+          _$ProjectStateImpl value, $Res Function(_$ProjectStateImpl) then) =
+      __$$ProjectStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String title, String image});
+}
+
+/// @nodoc
+class __$$ProjectStateImplCopyWithImpl<$Res>
+    extends _$ProjectStateCopyWithImpl<$Res, _$ProjectStateImpl>
+    implements _$$ProjectStateImplCopyWith<$Res> {
+  __$$ProjectStateImplCopyWithImpl(
+      _$ProjectStateImpl _value, $Res Function(_$ProjectStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of ProjectState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? image = null,
+  }) {
+    return _then(_$ProjectStateImpl(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      image: null == image
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$ProjectStateImpl implements _ProjectState {
+  const _$ProjectStateImpl({this.title = 0, this.image = 0});
+
+  @override
+  @JsonKey()
+  final String title;
+  @override
+  @JsonKey()
+  final String image;
+
+  @override
+  String toString() {
+    return 'ProjectState(title: $title, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ProjectStateImpl &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.image, image) || other.image == image));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, title, image);
+
+  /// Create a copy of ProjectState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ProjectStateImplCopyWith<_$ProjectStateImpl> get copyWith =>
+      __$$ProjectStateImplCopyWithImpl<_$ProjectStateImpl>(this, _$identity);
+}
+
+abstract class _ProjectState implements ProjectState {
+  const factory _ProjectState({final String title, final String image}) =
+      _$ProjectStateImpl;
+
+  @override
+  String get title;
+  @override
+  String get image;
+
+  /// Create a copy of ProjectState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ProjectStateImplCopyWith<_$ProjectStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
